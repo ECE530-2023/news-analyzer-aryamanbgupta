48cef930ba4575e26193cc368624ccbb1cc37683
Aryaman Gupta <aryaman@bu.edu>
2023-02-22 00:48:33 -0500

    Added stubs for FileUploader

---
+++ SecureFileUpload.py
@@ -0,0 +1,18 @@
+def AuthenticateUser():
+    #Authenticate user using login credentials
+
+    raise ValueError("Invalid login credentials")
+    return login_authentication
+
+def FileUpload():
+    #define File upload procedure for all valid file types
+
+    raise ValueError("Invalid File Type")
+    raise ValueError("File size too large")
+    return upload_status
+
+def EncryptFile():
+    #define procedure to encrypt files for upload
+
+    raise ValueError("Encryption Failed")
+    return successful_encryption
