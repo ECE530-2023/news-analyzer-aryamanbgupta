d9b56c807e71196a38e823bf52f91c4f5d53a2d4
Aryaman Gupta <aryaman@bu.edu>
2023-02-22 01:02:32 -0500

    added stubs for NLP and News Ingester

---
+++ NLPAnalysis.py
@@ -0,0 +1,18 @@
+def ExtractText():
+    #extract text from the uploaded document
+
+    raise ValueError("Text Extraction Failed")
+    return extracted_text
+
+def FindKeyWords():
+    #find key words from the text
+    #call NLP model
+
+    raise ValueError("Key Words not Found")
+    return key_words
+
+def SentimentAnalysis():
+    #Sentiment analysis on given text
+
+    raise ValueError("Sentiment Analysis Failed")
+    return sentiment_analysis_results
--- NewsIngester.py
+++ NewsIngester.py
@@ -3,3 +3,14 @@ from flask import Flask, request, jsonify
 app= Flask(__name__)
 
 
+def FindRelatedDocuments():
+    #find related documents from WEB using keywords
+
+    raise ValueError("Could not find related results")
+    return search_results
+
+def SummariseResults():
+    #use NLP model to get summaries of documents
+
+    raise ValueError("Summaries failes")
+    return summaries
