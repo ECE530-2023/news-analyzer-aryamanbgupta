c584340b23e820af5c1900d502dd848cf225ef6e
Aryaman Gupta <aryaman@bu.edu>
2023-03-19 19:05:19 -0400

    implemented database query in AuthenticateUser function

--- SecureFileUpload.py
+++ SecureFileUpload.py
@@ -1,8 +1,27 @@
-def AuthenticateUser():
-    #Authenticate user using login credentials
+import sqlite3
 
-    raise ValueError("Invalid login credentials")
+from matplotlib import use
+
+
+def AuthenticateUser(user_name,password):
+    #Authenticate user using login credentials
+    #Sanitize user input
+    sanitized_user_name= SanitizeInput(user_name)
+    sanitized_password= SanitizeInput(password)
+
+    #Connect to database and check credentials
+    conn = sqlite3.connect('pdf_reader.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (sanitized_user_name, sanitized_password))
+    row = c.fetchone()
+    conn.close()
+    if row is not None:
+        return True
+    else:
+        #If credentials are not found, raise an error
+        raise ValueError("Invalid login credentials")
     return login_authentication
+
 
 def FileUpload():
     #define File upload procedure for all valid file types
@@ -11,8 +30,15 @@ def FileUpload():
     raise ValueError("File size too large")
     return upload_status
 
+
 def EncryptFile():
     #define procedure to encrypt files for upload
 
     raise ValueError("Encryption Failed")
     return successful_encryption
+
+
+def SanitizeInput(input_str):
+    sanitized_str = input_str.strip()  # Remove leading and trailing whitespaces
+    sanitized_str = sanitized_str.replace("'", "''")  # Escape single quotes with double quotes
+    return sanitized_str
