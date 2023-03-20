de65b735155c7c9564aca155314cbee76bfdf1cb
Aryaman Gupta <aryaman@bu.edu>
2023-03-19 20:22:59 -0400

    implemented database insertion in FindRelatedDocuments function

--- NewsIngester.py
+++ NewsIngester.py
@@ -1,8 +1,22 @@
-def FindRelatedDocuments():
-    #find related documents from WEB using keywords
+import sqlite3
 
-    raise ValueError("Could not find related results")
-    return search_results
+
+def FindRelatedDocuments(keywords):
+    #find related documents from WEB using keywords
+    conn=sqlite3.connect('pdf_reader.db')
+    c=conn.cursor()
+
+    related_docs = set()
+    for keyword in keywords:
+        c.execute("SELECT DOCUMENT_ID FROM WEB_DOCUMENTS WHERE KEYWORDS LIKE ?", ('%' + keyword + '%',))
+        results = c.fetchall()
+        for row in results:
+            related_docs.add(row[0])
+
+    if related_docs is None:
+            raise ValueError("Could not find related results")
+    else:
+        return related_docs
 
 def SummariseResults():
     #use NLP model to get summaries of documents
--- SecureFileUpload.py
+++ SecureFileUpload.py
@@ -1,5 +1,4 @@
 import sqlite3
-import os
 import uuid
 from NLPAnalysis import ExtractText, FindKeyWords
 
