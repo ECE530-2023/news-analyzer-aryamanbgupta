b6cb45f6a780c0d47b0f4753e6d32600015c0a10
Aryaman Gupta <aryaman@bu.edu>
2023-05-04 15:13:50 -0400

    Added NLP functionality to find Key words

--- NLPAnalysis.py
+++ NLPAnalysis.py
@@ -1,4 +1,9 @@
 from pypdf import PdfReader
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
 
 def ExtractText(file_path):
     #extract text from the uploaded document
@@ -10,12 +15,30 @@ def ExtractText(file_path):
     #raise ValueError("Text Extraction Failed")
     return extracted_text
 
-def FindKeyWords():
+def FindKeyWords(total_text):
     #find key words from the text
-    #call NLP model
-    key_words=[]
+    #removing punctuation
+    total_text = total_text.translate(str.maketrans('', '', string.punctuation + "'" + '"'))
+
+    # Tokenize the text into individual words
+    words = word_tokenize(total_text)
+
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
+
+    # Calculate the frequency distribution of the words
+    freq_dist = nltk.FreqDist(lemmatized_words)
+
+    # Extract the most common keywords
+    top_keywords = [word for word, count in freq_dist.most_common(5)]
+
     #raise ValueError("Key Words not Found")
-    return key_words
+    return top_keywords
 
 def SentimentAnalysis():
     #Sentiment analysis on given text
--- UnitTest.py
+++ UnitTest.py
@@ -7,6 +7,10 @@ def test_ExtractText():
     res = ExtractText("Trial.pdf")
     assert res == "Trial 1 2 \\n  "
 
+def test_FindKeyWords():
+    text = "This is an example text to see if the program can find Key Words. The top 5 key words should be identified by the program in this example"
+    res = FindKeyWords(text)
+    assert res == ['example', 'program', 'text', 'see', 'find']
 '''
 
 def FileUploadTest():
