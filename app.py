ecbddd3d3e1da67dfea800971ba5d89bdfb993d2
Aryaman Gupta <aryaman@bu.edu>
2023-05-06 20:44:11 -0400

    added a login page to the app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from NLPAnalysis import ExtractText
+from SecureFileUpload import AuthenticateUser
 
 app = Flask(__name__)
 
@@ -18,3 +19,18 @@ def upload_file():
     text = ExtractText(file)
     # Do something with the uploaded file
     return f"<p> {text} </p>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        try:
+            user_id = AuthenticateUser(username, password)
+            return render_template('successful_login.html', user_id = user_id, username = username)
+        except ValueError:
+            return render_template('failed_login.html')
+
+    else:
+        return render_template('login.html')
