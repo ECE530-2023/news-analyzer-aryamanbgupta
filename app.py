53387b4133c31b9c1259979ee2e9425a58cddda8
Aryaman Gupta <aryaman@bu.edu>
2023-05-06 20:28:27 -0400

    integrating file upload to web app

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
+from NLPAnalysis import ExtractText
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/upload')
+def upload():
+    #print("hello")
+    return render_template('upload.html')
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    text = ExtractText(file)
+    # Do something with the uploaded file
+    return f"<p> {text} </p>"
